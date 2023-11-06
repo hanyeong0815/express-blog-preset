997aaeac0a4923c84d54d5f51f5d3e9155413237
hanyeong0815 <yoehadu815@gmail.com>
2023-11-06 21:26:35 +0900

    refactor(index ts): use config envvar instead of process.env

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import app from './app';
+import config from './config/config';
 
-const PORT = parseInt(process.env.PORT ?? '5000');
+const PORT = config.port();
 
 app.listen(PORT, () => {
   console.log(`Listenning port ${PORT}`);
