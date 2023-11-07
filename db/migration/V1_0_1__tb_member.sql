e52c196ccc1fbdad72332103781dacebc9fe8da5
hanyeong0815 <yoehadu815@gmail.com>
2023-11-07 23:09:11 +0900

    fix(db): fix db set(use postgresql, fix schema)

--- db/migration/V1_0_1__tb_member.sql
+++ db/migration/V1_0_1__tb_member.sql
@@ -1,11 +1,13 @@
 CREATE TABLE IF NOT EXISTS member.member (
   id              BIGSERIAL,
-  username        varchar(50),
-  password        varchar(255),
+  username        varchar(50)   NOT NULL,
+  password        varchar(255)  NOT NULL,
   email           varchar(255),
   gender_type     varchar(30),
   name            varchar(50),
   nickname        varchar(50),
 
-  constraint      pk_profile_id       primary key(id)
+  CONSTRAINT      pk_member_id              PRIMARY KEY(id),
+  CONSTRAINT      uq_member_username        UNIQUE(username),
+  CONSTRAINT      uq_member_email           UNIQUE(email)
 );
